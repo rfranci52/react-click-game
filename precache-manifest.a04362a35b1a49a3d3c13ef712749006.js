e7c842eca0fab71558329cdff201f0518539b1be
Rakim francis <rakimfrancis@Rakims-MBP-2.home>
2019-06-01 20:39:55 -0400

    Updates

---
+++ precache-manifest.a04362a35b1a49a3d3c13ef712749006.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc19a1b61a4fb698e4483a48eecf8f61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a5c2b0ab43ee509afda",
+    "url": "/static/css/main.0fdfa246.chunk.css"
+  },
+  {
+    "revision": "6ae27b8088d139983279",
+    "url": "/static/js/2.4aa49e51.chunk.js"
+  },
+  {
+    "revision": "9a5c2b0ab43ee509afda",
+    "url": "/static/js/main.e9731b4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
